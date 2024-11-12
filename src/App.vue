015270cbac76b3b3f049fc12f77808829c8a470d
Nites-Soni <nitesh.soni@ekshvaku.com>
2024-11-13 01:56:30 +0530

    Build created

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import GlobalLoader from 'vue-global-loader/GlobalLoader.vue'
 import CircleSpinner from 'vue-global-loader/CircleSpinner.vue'
 import { Notivue, Notifications } from 'notivue'
